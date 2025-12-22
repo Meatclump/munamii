37f6d41f087aff632a3195bdc7351542ddd80945
Meatclump <hepe22@hotmail.com>
2025-12-22 16:00:34 +0100

    Finished design. Need sample data

---
+++ src/components/products/products.js
@@ -0,0 +1,110 @@
+export const products = [
+    {
+        id: "001",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "002",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+    {
+        id: "003",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "004",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "005",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+    {
+        id: "006",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "007",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "008",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+    {
+        id: "018",
+        type: "cake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "009",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "010",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "011",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+    {
+        id: "012",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "013",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "014",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+    {
+        id: "015",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner3.jpg"
+    },
+    {
+        id: "016",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner2.jpg"
+    },
+    {
+        id: "017",
+        type: "cupcake",
+        name: "Product Name",
+        src: "/public/banner1.jpg"
+    },
+]
