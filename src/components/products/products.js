5ef6dcae07197740dfafa22bebca121cbf5364f6
Meatclump <hepe22@hotmail.com>
2025-12-23 10:14:11 +0100

    updated image paths

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,96 +3,96 @@ export const products = [
         id: "001",
         type: "cake",
         name: "Caramel Cake",
-        src: "/cakes/caramel-cake.jpg"
+        src: "/images/cakes/caramel-cake.jpg"
     },
     {
         id: "002",
         type: "cake",
         name: "Cheesecake",
-        src: "/cakes/cheesecake.jpg"
+        src: "/images/cakes/cheesecake.jpg"
     },
     {
         id: "003",
         type: "cake",
         name: "Chocolate Cake",
-        src: "/cakes/chocolate-cake.jpg"
+        src: "/images/cakes/chocolate-cake.jpg"
     },
     {
         id: "004",
         type: "cake",
         name: "Graham Cake",
-        src: "/cakes/graham-cake.jpg"
+        src: "/images/cakes/graham-cake.jpg"
     },
     {
         id: "005",
         type: "cake",
         name: "Red Velvet Cake",
-        src: "/cakes/red-velvet-cake.jpg"
+        src: "/images/cakes/red-velvet-cake.jpg"
     },
     {
         id: "006",
         type: "cake",
         name: "Roll Cake",
-        src: "/cakes/roll-cake.jpg"
+        src: "/images/cakes/roll-cake.jpg"
     },
     {
         id: "007",
         type: "cake",
         name: "Strawberry Cake",
-        src: "/cakes/strawberry-cake.jpg"
+        src: "/images/cakes/strawberry-cake.jpg"
     },
     {
         id: "008",
         type: "cake",
         name: "Wedding Cake",
-        src: "/cakes/wedding-cake.jpg"
+        src: "/images/cakes/wedding-cake.jpg"
     },
     {
         id: "009",
         type: "cupcake",
         name: "Birthday Cupcake",
-        src: "/cupcakes/birthday-cupcake.jpg"
+        src: "/images/cupcakes/birthday-cupcake.jpg"
     },
     {
         id: "010",
         type: "cupcake",
         name: "Blueberry Cupcake",
-        src: "/cupcakes/blueberry-cupcake.jpg"
+        src: "/images/cupcakes/blueberry-cupcake.jpg"
     },
     {
         id: "011",
         type: "cupcake",
         name: "Blue Cupcake",
-        src: "/cupcakes/blue-cupcake.jpg"
+        src: "/images/cupcakes/blue-cupcake.jpg"
     },
     {
         id: "012",
         type: "cupcake",
         name: "Chocolate Cupcake",
-        src: "/cupcakes/chocolate-cupcake.jpg"
+        src: "/images/cupcakes/chocolate-cupcake.jpg"
     },
     {
         id: "013",
         type: "cupcake",
         name: "Halloween Cupcake",
-        src: "/cupcakes/halloween-cupcake.jpg"
+        src: "/images/cupcakes/halloween-cupcake.jpg"
     },
     {
         id: "014",
         type: "cupcake",
         name: "Orange Cupcake",
-        src: "/cupcakes/orange-cupcake.jpg"
+        src: "/images/cupcakes/orange-cupcake.jpg"
     },
     {
         id: "015",
         type: "cupcake",
         name: "Oreo Cupcake",
-        src: "/cupcakes/oreo-cupcake.jpg"
+        src: "/images/cupcakes/oreo-cupcake.jpg"
     },
     {
         id: "016",
         type: "cupcake",
         name: "Strawberry Cupcake",
-        src: "/cupcakes/strawberry-cupcake.jpg"
+        src: "/images/cupcakes/strawberry-cupcake.jpg"
     },
 ]
